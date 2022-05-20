f9cd65a0a0c0043ab284b90b93edbd8153bd3920
Nika <nikaantidze4@gmail.com>
2022-05-21 01:42:32 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,79 +1,78 @@
-import './App.css';
-import React, {useState} from 'react';
-import axios from 'axios';
-
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
-
-const [data,setData] = useState({});
-const [location,setLocation] = useState('');
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=ce75cc7f5965ad830e2387b715e7e358`;
-  function searchLocation(event) {
-    if (event.key === 'Enter') {
+  function searchLocation(e) {
+    if (e.key === "Enter") {
       axios.get(url).then((response) => {
         setData(response.data);
-        console.log(response.data);
-      })
-      setLocation('')
+      });
+      setLocation("");
     }
   }
 
   return (
     <div className="App relative w-full h-[100vh] text-white bg-[rgba(0,0,0,0.4)]">
-      <div className='search text-center p-[1rem]'>
+      <div className="search text-center p-[1rem]">
         <input
           value={location}
-          onChange={event => setLocation(event.target.value)}
+          onChange={(e) => setLocation(e.target.value)}
           placeholder="Enter Location"
           onKeyPress={searchLocation}
           type="text"
         />
       </div>
 
-        <div className='container max-w-[700px] h-[700px] m-auto p-[0 1rem] relative top-[10%] flex flex-col justify-between'>
-          <div className='top'>
-
-            <div className='location'>
-              <p>{data.name}</p>
-              
-              <div className='temp'>
-                {data.main ? <h1>{data.main.temp.toFixed()} °C</h1> : null}
-            </div>
-
-            <div className='description'>
-              {data.weather ? <p>{data.weather[0].main}</p> : null}
-            </div>
-
+      <div className="container max-w-[700px] h-[700px] m-auto p-[0 1rem] relative top-[10%] flex flex-col justify-between">
+        <div className="top sm:px-7">
+          <div className="location">
+            <p>{data.name}</p>
+            {data.main && (
+              <div className="temp">
+                <h1>{data.main.temp.toFixed()} °C</h1>
+              </div>
+            )}
+            {data.weather && (
+              <div className="description">
+                <p>{data.weather[0].main}</p>
+              </div>
+            )}
           </div>
-
-          </div>
-       
-          {data.name !== undefined &&
-          
-             <div className='bottom'>
-
-                <div className='feels'>
-                  {data.main ? <p className="font-bold flex justify-center">{data.main.feels_like.toFixed()} °C</p> : null}
-                  <p className='flex justify-center'>Feels like</p>
-                </div>
-
-                <div className='humidity'>
-                  {data.main ? <p className="font-bold flex justify-center">{data.main.humidity}%</p> : null}
-                  
-                  <p className='flex justify-center'>Humidity</p>
-                </div>
-
-                <div className='wind'>
-                  {data.wind ? <p className="font-bold flex justify-center">{data.wind.speed.toFixed()} MPH</p> : null}
-                  <p className='flex justify-center'>Wind</p>
-                </div>
-
-          </div>
-
-          }
-
         </div>
+        {data.name !== undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main && (
+                <p className="font-bold flex justify-center">
+                  {data.main.feels_like.toFixed()} °C
+                </p>
+              )}
+              <p className="flex justify-center">Feels like</p>
+            </div>
+            <div className="humidity">
+              {data.main && (
+                <p className="font-bold flex justify-center">
+                  {data.main.humidity}%
+                </p>
+              )}
+              <p className="flex justify-center">Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind && (
+                <p className="font-bold flex justify-center">
+                  {data.wind.speed.toFixed()} MPH
+                </p>
+              )}
+              <p className="flex justify-center">Wind</p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
